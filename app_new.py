a4955f7169093e57a60a52859ae426b949215ffe
prowentech <prowentech@gmail.com>
2026-01-14 18:18:46 +0530

    Fix server run

--- app_new.py
+++ app_new.py
@@ -306,8 +306,6 @@ def webhook():
         return "EVENT_RECEIVED", 200
     
 
-
-
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    # app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(debug=False)
